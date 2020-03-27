9ae4675975cafefd9f2db832f63c7f9d4d53cf6a
mohammed s. yaghi <yaghi.moh@gmail.com>
2020-03-27 15:26:35 +0300

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './Footer.css'
+export default class Footer extends React.Component{
+
+    render() {
+
+        return (<div className="footer-section">Footer Data<p className="x">Hello</p></div>)
+    }
+
+
+}
